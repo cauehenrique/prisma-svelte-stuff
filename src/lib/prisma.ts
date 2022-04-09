0943c987a886128290b53f796076476a6f878ae9
cauehenrique <caue313@gmail.com>
2022-04-09 09:21:38 -0300

    update: PrismaClient import

---
+++ src/lib/prisma.ts
@@ -0,0 +1,4 @@
+import Prisma from '@prisma/client'
+
+const prisma = new Prisma.PrismaClient()
+export default prisma
--- src/routes/api/clients.ts
+++ src/routes/api/clients.ts
@@ -1,7 +1,5 @@
-import { PrismaClient } from '@prisma/client'
+import prisma from '$lib/prisma'
 import type { RequestHandler } from '@sveltejs/kit'
-
-const prisma = new PrismaClient()
 
 export const get: RequestHandler = async () => {
 	const clients = await prisma.client.findMany()
